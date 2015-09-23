b8281e3c9f0c546e37897991f68692142dfd3201
Hea3veN <hea3venmc@gmail.com>
2015-09-23 20:17:51 -0300

    Fix blocks third-person rendering size

--- src/main/java/com/hea3ven/buildingbricks/core/client/model/ModelItemMaterialBlock.java
+++ src/main/java/com/hea3ven/buildingbricks/core/client/model/ModelItemMaterialBlock.java
@@ -2,6 +2,10 @@ package com.hea3ven.buildingbricks.core.client.model;
 
 import java.util.HashMap;
 
+import javax.vecmath.Vector3f;
+
+import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
+import net.minecraft.client.renderer.block.model.ItemTransformVec3f;
 import net.minecraft.client.resources.model.IBakedModel;
 import net.minecraft.item.ItemStack;
 
@@ -11,6 +15,15 @@ import net.minecraftforge.common.util.Constants.NBT;
 
 @SuppressWarnings("deprecation")
 public class ModelItemMaterialBlock extends DelegatedSmartModel implements ISmartItemModel {
+
+	private static ItemCameraTransforms cameraTransforms = new ItemCameraTransforms(
+			new ItemTransformVec3f(new Vector3f(-20, 135, 180),
+					new Vector3f(0, 1.5f * 0.0625f, -2.75f * 0.0625f),
+					new Vector3f(0.375f, 0.375f, 0.375f)),
+			new ItemTransformVec3f(new Vector3f(0, 180, 0), new Vector3f(0, 0, 0),
+					new Vector3f(1f, 1f, 1f)),
+			new ItemTransformVec3f(new Vector3f(), new Vector3f(), new Vector3f(1, 1, 1)),
+			new ItemTransformVec3f(new Vector3f(), new Vector3f(), new Vector3f(1, 1, 1)));
 
 	private HashMap<String, ModelItemMaterialBlock> models;
 
@@ -36,4 +49,8 @@ public class ModelItemMaterialBlock extends DelegatedSmartModel implements ISmar
 			return this;
 	}
 
+	@Override
+	public ItemCameraTransforms getItemCameraTransforms() {
+		return cameraTransforms;
+	}
 }
