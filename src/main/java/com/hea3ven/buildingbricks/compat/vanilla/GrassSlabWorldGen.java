0cf4b448650b22847f3c087cabdc19bcc897a67b
Hea3veN <hea3venmc@gmail.com>
2015-11-25 20:48:18 -0300

    Fix grass slabs world gen generating chunks recursively, fixes #22

--- src/main/java/com/hea3ven/buildingbricks/compat/vanilla/GrassSlabWorldGen.java
+++ src/main/java/com/hea3ven/buildingbricks/compat/vanilla/GrassSlabWorldGen.java
@@ -3,6 +3,8 @@ package com.hea3ven.buildingbricks.compat.vanilla;
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.world.chunk.IChunkProvider;
 
 import net.minecraftforge.event.terraingen.PopulateChunkEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -14,58 +16,66 @@ public class GrassSlabWorldGen {
 
 	@SubscribeEvent
 	public void onPopulateChunkPreEvent(PopulateChunkEvent.Pre event) {
-		for (ModifiableBlockPos pos : BlockPosUtil
-				.getBox(new BlockPos(event.chunkX << 4, 0, event.chunkZ << 4), 16, 1, 16)) {
+		IChunkProvider chunkProvider = event.world.getChunkProvider();
+		int x = event.chunkX << 4;
+		int z = event.chunkZ << 4;
+		int offX = 16;
+		int offZ = 16;
+		if (chunkProvider.chunkExists(event.chunkX - 1, event.chunkZ)) {
+			x--;
+			offX++;
+		} else {
+			x++;
+			offX--;
+		}
+		if (chunkProvider.chunkExists(event.chunkX, event.chunkZ - 1)) {
+			z--;
+			offZ++;
+		} else {
+			z++;
+			offZ--;
+		}
+		if (chunkProvider.chunkExists(event.chunkX + 1, event.chunkZ))
+			offX++;
+		else
+			offX--;
+		if (chunkProvider.chunkExists(event.chunkX, event.chunkZ + 1))
+			offZ++;
+		else
+			offZ--;
+		posLoop:
+		for (ModifiableBlockPos pos : BlockPosUtil.getBox(new BlockPos(x, 0, z), offX, 1, offZ)) {
 			while (pos.getY() < 255 && event.world.getBlockState(pos).getBlock() != Blocks.grass) {
 				pos.up();
 			}
-			if (pos.getY() >= 255)
+			if (pos.getY() >= 255) {
+				if (event.chunkX == -4 && event.chunkZ == 4)
+					System.out.println("ignoring pos " + pos.toString());
 				continue;
+			}
 
-			pos.north();
-			Block block = event.world.getBlockState(pos).getBlock();
-			if (block.isReplaceable(event.world, pos)
-					|| block == ModBuildingBricksCompatVanilla.grassSlab)
-				continue;
-			pos.south(2);
-			block = event.world.getBlockState(pos).getBlock();
-			if (block.isReplaceable(event.world, pos)
-					|| block == ModBuildingBricksCompatVanilla.grassSlab)
-				continue;
-			pos.north();
-			pos.east();
-			block = event.world.getBlockState(pos).getBlock();
-			if (block.isReplaceable(event.world, pos)
-					|| block == ModBuildingBricksCompatVanilla.grassSlab)
-				continue;
-			pos.west(2);
-			block = event.world.getBlockState(pos).getBlock();
-			if (block.isReplaceable(event.world, pos)
-					|| block == ModBuildingBricksCompatVanilla.grassSlab)
-				continue;
-			pos.east();
+			if (event.chunkX == -4 && event.chunkZ == 4)
+				System.out.println("testing pos " + pos.toString());
+			for (EnumFacing face : EnumFacing.HORIZONTALS) {
+				pos.offset(face, 1);
+				Block block = event.world.getBlockState(pos).getBlock();
+				if (block.isReplaceable(event.world, pos) ||
+						block == ModBuildingBricksCompatVanilla.grassSlab)
+					continue posLoop;
+				pos.offset(face, -1);
+			}
 
 			pos.up();
 
-			if (event.world.getBlockState(pos.north()).getBlock() == Blocks.grass) {
-				event.world.setBlockState(pos.south(),
-						ModBuildingBricksCompatVanilla.grassSlab.getDefaultState(), 2);
-				continue;
-			}
-			if (event.world.getBlockState(pos.south(2)).getBlock() == Blocks.grass) {
-				event.world.setBlockState(pos.north(),
-						ModBuildingBricksCompatVanilla.grassSlab.getDefaultState(), 2);
-				continue;
-			}
-			pos.north();
-			if (event.world.getBlockState(pos.east()).getBlock() == Blocks.grass) {
-				event.world.setBlockState(pos.west(),
-						ModBuildingBricksCompatVanilla.grassSlab.getDefaultState(), 2);
-				continue;
-			}
-			if (event.world.getBlockState(pos.west(2)).getBlock() == Blocks.grass) {
-				event.world.setBlockState(pos.east(),
-						ModBuildingBricksCompatVanilla.grassSlab.getDefaultState(), 2);
+			for (EnumFacing face : EnumFacing.HORIZONTALS) {
+				pos.offset(face, 1);
+				if (event.world.getBlockState(pos).getBlock() == Blocks.grass) {
+					pos.offset(face, -1);
+					event.world.setBlockState(pos, ModBuildingBricksCompatVanilla.grassSlab.getDefaultState(),
+							2);
+					continue posLoop;
+				}
+				pos.offset(face, -1);
 			}
 		}
 	}
