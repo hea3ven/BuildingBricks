43e7846d82dc380d3ef28d4b295d08d3c2a7ff39
Hea3veN <hea3venmc@gmail.com>
2016-03-01 20:40:20 -0300

    Update bootstrap

---
+++ src/main/java/com/hea3ven/buildingbricks/compat/jei/ProxyModBuildingBricksCompatJei.java
@@ -0,0 +1,18 @@
+package com.hea3ven.buildingbricks.compat.jei;
+
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+
+import com.hea3ven.tools.commonutils.mod.ProxyModBase;
+
+public class ProxyModBuildingBricksCompatJei extends ProxyModBase {
+	public ProxyModBuildingBricksCompatJei(String modId) {
+		super(modId);
+	}
+
+	@Override
+	public void onPostInitEvent(FMLPostInitializationEvent event) {
+		super.onPostInitEvent(event);
+
+
+	}
+}
