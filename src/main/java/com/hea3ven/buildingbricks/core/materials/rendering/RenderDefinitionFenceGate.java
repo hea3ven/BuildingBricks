142b8861cee3764abe675bc37a9c04f22968c0a4
Hea3veN <hea3venmc@gmail.com>
2016-04-27 22:16:29 -0300

    * Fix the fence gate model not being rotated correctly

--- src/main/java/com/hea3ven/buildingbricks/core/materials/rendering/RenderDefinitionFenceGate.java
+++ src/main/java/com/hea3ven/buildingbricks/core/materials/rendering/RenderDefinitionFenceGate.java
@@ -3,8 +3,10 @@ package com.hea3ven.buildingbricks.core.materials.rendering;
 import net.minecraft.block.BlockFenceGate;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelManager;
+import net.minecraft.client.renderer.block.model.ModelRotation;
 
 import net.minecraftforge.client.model.IModel;
+import net.minecraftforge.common.model.IModelState;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -24,5 +26,20 @@ public class RenderDefinitionFenceGate extends RenderDefinitionSimple {
 			return loadModel(modelManager, "block", mat, "fence_gate_open");
 		else
 			return loadModel(modelManager, "block", mat, "fence_gate_closed");
+	}
+
+	@Override
+	protected IModelState getModelState(IBlockState state) {
+		switch (state.getValue(BlockFenceGate.FACING)) {
+			default:
+			case NORTH:
+				return ModelRotation.X0_Y0;
+			case EAST:
+				return ModelRotation.X0_Y90;
+			case SOUTH:
+				return ModelRotation.X0_Y180;
+			case WEST:
+				return ModelRotation.X0_Y270;
+		}
 	}
 }
