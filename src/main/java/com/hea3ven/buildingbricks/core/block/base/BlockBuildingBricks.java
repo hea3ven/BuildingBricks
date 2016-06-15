fc66cdd93802ecda3794c94d9b096e9351dd3893
Hea3veN <hea3venmc@gmail.com>
2016-06-15 10:42:17 -0300

    Clean up the branch

--- src/main/java/com/hea3ven/buildingbricks/core/block/base/BlockBuildingBricks.java
+++
@@ -1,9 +0,0 @@
-package com.hea3ven.buildingbricks.core.block.base;
-
-import com.hea3ven.buildingbricks.core.materials.MaterialBlockLogic;
-
-public interface BlockBuildingBricks {
-
-	MaterialBlockLogic getBlockLogic();
-
-}
