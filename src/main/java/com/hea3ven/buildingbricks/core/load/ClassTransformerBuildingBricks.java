b83e090f121c9c801a8b6d7c3b89f9d4860b4d8a
Hea3veN <hea3venmc@gmail.com>
2016-11-06 09:03:28 -0300

    * Update to new asmtweaks API

--- src/main/java/com/hea3ven/buildingbricks/core/load/ClassTransformerBuildingBricks.java
+++ src/main/java/com/hea3ven/buildingbricks/core/load/ClassTransformerBuildingBricks.java
@@ -2,25 +2,23 @@ package com.hea3ven.buildingbricks.core.load;
 
 import net.minecraft.launchwrapper.IClassTransformer;
 
-import com.hea3ven.tools.asmtweaks.ASMTweaksManager;
-import com.hea3ven.tools.asmtweaks.ASMTweaksManagerBuilder;
+import com.hea3ven.tools.asmtweaks.ASMTweaksBuilder;
+import com.hea3ven.tools.asmtweaks.ASMTweaksRunner;
 
 public class ClassTransformerBuildingBricks implements IClassTransformer {
 
-	ASMTweaksManager tweaksManager =
-			new ASMTweaksManagerBuilder()
-					.loadMappings("/mappings")
-					.addMthdSrg("net/minecraft/item/ItemBlock/onItemUse",
-							"net/minecraft/item/ItemBlock/func_180614_a",
-							"(Lnet/minecraft/item/ItemStack;Lnet/minecraft/entity/player/EntityPlayer;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/EnumHand;Lnet/minecraft/util/EnumFacing;FFF)Lnet/minecraft/util/EnumActionResult;")
-					.addMthdSrg("net/minecraft/item/ItemBlock/canPlaceBlockOnSide",
-							"net/minecraft/item/ItemBlock/func_179222_a",
-							"(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/EnumFacing;Lnet/minecraft/entity/player/EntityPlayer;Lnet/minecraft/item/ItemStack;)Z")
-					.addTweak(new ItemBlockPlaceOverride())
-					.build();
+	ASMTweaksRunner tweaksRunner = ASMTweaksBuilder.create()
+			.addMthdSrg("net/minecraft/item/ItemBlock.onItemUse",
+					"net/minecraft/item/ItemBlock.func_180614_a",
+					"(Lnet/minecraft/item/ItemStack;Lnet/minecraft/entity/player/EntityPlayer;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/EnumHand;Lnet/minecraft/util/EnumFacing;FFF)Lnet/minecraft/util/EnumActionResult;")
+			.addMthdSrg("net/minecraft/item/ItemBlock.canPlaceBlockOnSide",
+					"net/minecraft/item/ItemBlock.func_179222_a",
+					"(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/EnumFacing;Lnet/minecraft/entity/player/EntityPlayer;Lnet/minecraft/item/ItemStack;)Z")
+			.addTweak(new ItemBlockPlaceOverride())
+			.build();
 
 	@Override
 	public byte[] transform(String name, String transformedName, byte[] basicClass) {
-		return tweaksManager.transform(name, transformedName, basicClass);
+		return tweaksRunner.transform(name, transformedName, basicClass);
 	}
 }
--- src/main/java/com/hea3ven/buildingbricks/core/load/ItemBlockPlaceOverride.java
+++ src/main/java/com/hea3ven/buildingbricks/core/load/ItemBlockPlaceOverride.java
@@ -3,13 +3,14 @@ package com.hea3ven.buildingbricks.core.load;
 import java.util.Set;
 
 import com.google.common.collect.Sets;
-import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.tree.LabelNode;
 
 import com.hea3ven.tools.asmtweaks.ASMMod;
 import com.hea3ven.tools.asmtweaks.ASMTweak;
 import com.hea3ven.tools.asmtweaks.ASMTweaksConfig.ASMTweakConfig;
+import com.hea3ven.tools.asmtweaks.editors.ASMContext;
+import com.hea3ven.tools.asmtweaks.editors.LabelRef;
 import com.hea3ven.tools.asmtweaks.editors.MethodEditor;
+import com.hea3ven.tools.asmtweaks.editors.opcodes.*;
 import com.hea3ven.tools.asmtweaks.tweaks.ASMMethodModEditCode;
 
 public class ItemBlockPlaceOverride implements ASMTweak {
@@ -30,58 +31,62 @@ public class ItemBlockPlaceOverride implements ASMTweak {
 	}
 
 	static {
-		modifications.add(new ASMMethodModEditCode("net/minecraft/item/ItemBlock/onItemUse",
+		modifications.add(new ASMMethodModEditCode("net/minecraft/item/ItemBlock.onItemUse",
 				"(Lnet/minecraft/item/ItemStack;Lnet/minecraft/entity/player/EntityPlayer;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/EnumHand;Lnet/minecraft/util/EnumFacing;FFF)Lnet/minecraft/util/EnumActionResult;") {
 			@Override
 			protected void handle(MethodEditor editor) {
-//				ItemBlockUseEvent event =
-// 						new ItemBlockUseEvent(stack, player, world, pos, hand, side, hitX, hitY, hitZ);
-//				if(MinecraftForge.EVENT_BUS.post(event)) return event.getActionResult();
+				// ItemBlockUseEvent event =
+				// 		new ItemBlockUseEvent(stack, player, world, pos, hand, side, hitX, hitY, hitZ);
+				// if(MinecraftForge.EVENT_BUS.post(event)) return event.getActionResult();
 
-				editor.addImport("net/minecraft/item/ItemStack");
-				editor.addImport("net/minecraft/entity/player/EntityPlayer");
-				editor.addImport("net/minecraft/util/math/BlockPos");
-				editor.addImport("net/minecraft/util/EnumHand");
-				editor.addImport("net/minecraft/util/EnumFacing");
-				editor.addImport("net/minecraft/util/EnumActionResult");
-				editor.addImport("net/minecraft/world/World");
-				editor.addImport("net/minecraftforge/common/MinecraftForge");
-				editor.addImport("net/minecraftforge/fml/common/eventhandler/Event");
-				editor.addImport("net/minecraftforge/fml/common/eventhandler/EventBus");
+				ASMContext ctx = new ASMContext();
+				ctx.addImport("net/minecraft/item/ItemStack");
+				ctx.addImport("net/minecraft/entity/player/EntityPlayer");
+				ctx.addImport("net/minecraft/util/math/BlockPos");
+				ctx.addImport("net/minecraft/util/EnumHand");
+				ctx.addImport("net/minecraft/util/EnumFacing");
+				ctx.addImport("net/minecraft/util/EnumActionResult");
+				ctx.addImport("net/minecraft/world/World");
+				ctx.addImport("net/minecraftforge/common/MinecraftForge");
+				ctx.addImport("net/minecraftforge/fml/common/eventhandler/Event");
+				ctx.addImport("net/minecraftforge/fml/common/eventhandler/EventBus");
 
-				editor.addImport("com/hea3ven/buildingbricks/core/block/placement/ItemBlockUseEvent");
+				ctx.addImport("com/hea3ven/buildingbricks/core/block/placement/ItemBlockUseEvent");
 
-				LabelNode lbl = new LabelNode();
+				LabelRef lbl = editor.createLabel();
 
 				editor.setInsertMode();
-				editor.typeInsn(Opcodes.NEW, "ItemBlockUseEvent");
-				editor.insn(Opcodes.DUP);
-				editor.varInsn(Opcodes.ALOAD, 1);
-				editor.varInsn(Opcodes.ALOAD, 2);
-				editor.varInsn(Opcodes.ALOAD, 3);
-				editor.varInsn(Opcodes.ALOAD, 4);
-				editor.varInsn(Opcodes.ALOAD, 5);
-				editor.varInsn(Opcodes.ALOAD, 6);
-				editor.varInsn(Opcodes.FLOAD, 7);
-				editor.varInsn(Opcodes.FLOAD, 8);
-				editor.varInsn(Opcodes.FLOAD, 9);
-				editor.methodInsn(Opcodes.INVOKESPECIAL, "ItemBlockUseEvent",
-						"ItemBlockUseEvent/<init>",
-						"(LItemStack;LEntityPlayer;LWorld;LBlockPos;LEnumHand;LEnumFacing;FFF)V");
-				editor.varInsn(Opcodes.ASTORE, 10);
-				editor.fieldInsn(Opcodes.GETSTATIC, "MinecraftForge", "MinecraftForge/EVENT_BUS",
-						"LEventBus;");
-				editor.varInsn(Opcodes.ALOAD, 10);
-				editor.methodInsn(Opcodes.INVOKEVIRTUAL, "EventBus", "EventBus/post", "(LEvent;)Z");
-				editor.jumpInsn(Opcodes.IFEQ, lbl);
-				editor.varInsn(Opcodes.ALOAD, 10);
-				editor.methodInsn(Opcodes.INVOKEVIRTUAL, "ItemBlockUseEvent",
-						"ItemBlockUseEvent/getActionResult", "()LEnumActionResult;");
-				editor.insn(Opcodes.ARETURN);
-				editor.labelInsn(lbl);
+				editor.apply(
+						editor.newInstructionBuilder(ctx)
+								.typeInsn(TypeInsnOpcodes.NEW, "LItemBlockUseEvent;")
+								.insn(InsnOpcodes.DUP)
+								.varInsn(VarInsnOpcodes.ALOAD, 1)
+								.varInsn(VarInsnOpcodes.ALOAD, 2)
+								.varInsn(VarInsnOpcodes.ALOAD, 3)
+								.varInsn(VarInsnOpcodes.ALOAD, 4)
+								.varInsn(VarInsnOpcodes.ALOAD, 5)
+								.varInsn(VarInsnOpcodes.ALOAD, 6)
+								.varInsn(VarInsnOpcodes.FLOAD, 7)
+								.varInsn(VarInsnOpcodes.FLOAD, 8)
+								.varInsn(VarInsnOpcodes.FLOAD, 9)
+								.methodInsn(MethodInsnOpcodes.INVOKESPECIAL, "ItemBlockUseEvent",
+										"ItemBlockUseEvent.<init>",
+										"(LItemStack;LEntityPlayer;LWorld;LBlockPos;LEnumHand;LEnumFacing;FFF)V")
+								.varInsn(VarInsnOpcodes.ASTORE, 10)
+								.fieldInsn(FieldInsnOpcodes.GETSTATIC, "MinecraftForge",
+										"MinecraftForge.EVENT_BUS", "LEventBus;")
+								.varInsn(VarInsnOpcodes.ALOAD, 10)
+								.methodInsn(MethodInsnOpcodes.INVOKEVIRTUAL, "EventBus", "EventBus.post",
+										"(LEvent;)Z")
+								.jumpInsn(JumpInsnOpcodes.IFEQ, lbl)
+								.varInsn(VarInsnOpcodes.ALOAD, 10)
+								.methodInsn(MethodInsnOpcodes.INVOKEVIRTUAL, "ItemBlockUseEvent",
+										"ItemBlockUseEvent.getActionResult", "()LEnumActionResult;")
+								.insn(InsnOpcodes.ARETURN)
+								.label(lbl));
 			}
 		});
-		modifications.add(new ASMMethodModEditCode("net/minecraft/item/ItemBlock/canPlaceBlockOnSide",
+		modifications.add(new ASMMethodModEditCode("net/minecraft/item/ItemBlock.canPlaceBlockOnSide",
 				"(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/EnumFacing;Lnet/minecraft/entity/player/EntityPlayer;Lnet/minecraft/item/ItemStack;)Z") {
 
 			@Override
@@ -91,48 +96,57 @@ public class ItemBlockPlaceOverride implements ASMTweak {
 
 			@Override
 			protected void handle(MethodEditor editor) {
+				handle(editor, true);
+			}
 
+			protected boolean handle(MethodEditor editor, boolean asd) {
 //				ItemBlockCanPlaceBlockOnSideEvent event =
 //						new ItemBlockCanPlaceBlockOnSideEvent(world, pos, side, player, stack);
-//				if(MinecraftForge.EVENT_BUS.post(event)) return event.isCanPlaceBlockOnSide();
+//				if (MinecraftForge.EVENT_BUS.post(event))
+//					return event.isCanPlaceBlockOnSide();
 
-				editor.addImport("net/minecraft/item/ItemStack");
-				editor.addImport("net/minecraft/entity/player/EntityPlayer");
-				editor.addImport("net/minecraft/util/math/BlockPos");
-				editor.addImport("net/minecraft/util/EnumFacing");
-				editor.addImport("net/minecraft/world/World");
-				editor.addImport("net/minecraftforge/common/MinecraftForge");
-				editor.addImport("net/minecraftforge/fml/common/eventhandler/Event");
-				editor.addImport("net/minecraftforge/fml/common/eventhandler/EventBus");
+				ASMContext ctx = new ASMContext();
+				ctx.addImport("net/minecraft/item/ItemStack");
+				ctx.addImport("net/minecraft/entity/player/EntityPlayer");
+				ctx.addImport("net/minecraft/util/math/BlockPos");
+				ctx.addImport("net/minecraft/util/EnumFacing");
+				ctx.addImport("net/minecraft/world/World");
+				ctx.addImport("net/minecraftforge/common/MinecraftForge");
+				ctx.addImport("net/minecraftforge/fml/common/eventhandler/Event");
+				ctx.addImport("net/minecraftforge/fml/common/eventhandler/EventBus");
 
-				editor.addImport(
+				ctx.addImport(
 						"com/hea3ven/buildingbricks/core/block/placement/ItemBlockCanPlaceBlockOnSideEvent");
 
-				LabelNode lbl = new LabelNode();
+				LabelRef lbl = editor.createLabel();
 
 				editor.setInsertMode();
-				editor.typeInsn(Opcodes.NEW, "ItemBlockCanPlaceBlockOnSideEvent");
-				editor.insn(Opcodes.DUP);
-				editor.varInsn(Opcodes.ALOAD, 1);
-				editor.varInsn(Opcodes.ALOAD, 2);
-				editor.varInsn(Opcodes.ALOAD, 3);
-				editor.varInsn(Opcodes.ALOAD, 4);
-				editor.varInsn(Opcodes.ALOAD, 5);
-				editor.methodInsn(Opcodes.INVOKESPECIAL, "ItemBlockCanPlaceBlockOnSideEvent",
-						"ItemBlockCanPlaceBlockOnSideEvent/<init>",
-						"(LWorld;LBlockPos;LEnumFacing;LEntityPlayer;LItemStack;)V");
-				editor.varInsn(Opcodes.ASTORE, 6);
-				editor.fieldInsn(Opcodes.GETSTATIC, "MinecraftForge", "MinecraftForge/EVENT_BUS",
-						"LEventBus;");
-				editor.varInsn(Opcodes.ALOAD, 6);
-				editor.methodInsn(Opcodes.INVOKEVIRTUAL, "EventBus", "EventBus/post", "(LEvent;)Z");
-				editor.jumpInsn(Opcodes.IFEQ, lbl);
-				editor.varInsn(Opcodes.ALOAD, 6);
-				editor.methodInsn(Opcodes.INVOKEVIRTUAL, "ItemBlockCanPlaceBlockOnSideEvent",
-						"ItemBlockCanPlaceBlockOnSideEvent/isCanPlaceBlockOnSide", "()Z");
-				editor.insn(Opcodes.IRETURN);
-				editor.labelInsn(lbl);
+				editor.apply(editor.newInstructionBuilder(ctx)
+						.typeInsn(TypeInsnOpcodes.NEW, "LItemBlockCanPlaceBlockOnSideEvent;")
+						.insn(InsnOpcodes.DUP)
+						.varInsn(VarInsnOpcodes.ALOAD, 1)
+						.varInsn(VarInsnOpcodes.ALOAD, 2)
+						.varInsn(VarInsnOpcodes.ALOAD, 3)
+						.varInsn(VarInsnOpcodes.ALOAD, 4)
+						.varInsn(VarInsnOpcodes.ALOAD, 5)
+						.methodInsn(MethodInsnOpcodes.INVOKESPECIAL, "ItemBlockCanPlaceBlockOnSideEvent",
+								"ItemBlockCanPlaceBlockOnSideEvent.<init>",
+								"(LWorld;LBlockPos;LEnumFacing;LEntityPlayer;LItemStack;)V")
+						.varInsn(VarInsnOpcodes.ASTORE, 7)
+						.fieldInsn(FieldInsnOpcodes.GETSTATIC, "MinecraftForge", "MinecraftForge.EVENT_BUS",
+								"LEventBus;")
+						.varInsn(VarInsnOpcodes.ALOAD, 7)
+						.methodInsn(MethodInsnOpcodes.INVOKEVIRTUAL, "EventBus", "EventBus.post",
+								"(LEvent;)Z")
+						.jumpInsn(JumpInsnOpcodes.IFEQ, lbl)
+						.varInsn(VarInsnOpcodes.ALOAD, 7)
+						.methodInsn(MethodInsnOpcodes.INVOKEVIRTUAL, "ItemBlockCanPlaceBlockOnSideEvent",
+								"ItemBlockCanPlaceBlockOnSideEvent.isCanPlaceBlockOnSide", "()Z")
+						.insn(InsnOpcodes.IRETURN)
+						.label(lbl));
+				return true;
 			}
 		});
 	}
 }
+
